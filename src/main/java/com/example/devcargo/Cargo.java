f15487459d6c5214462d515303c3e4b3ea3050ac
anna <koychieva.anna@mail.ru>
2023-11-30 21:05:39 +0300

    Кото-названия

--- src/main/java/com/example/devcargo/Cargo.java
+++ src/main/java/com/example/devcargo/Cargo.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+
 import java.util.Date;
 
+@Getter
 @Entity
 public class Cargo {
 
@@ -23,56 +26,28 @@ public class Cargo {
     protected Cargo() {
     }
 
-    public Long getId() {
-        return this.id;
-    }
-
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getContent() {
-        return this.content;
-    }
-
     public void setContent(String content) {
         this.content = content;
-    }
-
-    public String getDepartureCity() {
-        return this.departureCity;
     }
 
     public void setDepartureCity(String departureCity) {
         this.departureCity = departureCity;
     }
 
-    public Date getDepartureDate() {
-        return this.departureDate;
-    }
-
     public void setDepartureDate(Date departureDate) {
         this.departureDate = departureDate;
     }
 
-    public String getArrivalCity() {
-        return this.arrivalCity;
-    }
-
     public void setArrivalCity(String arrivalCity) {
         this.arrivalCity = arrivalCity;
-    }
-
-    public Date getArrivalDate() {
-        return this.arrivalDate;
     }
 
     public void setArrivalDate(Date arrivalDate) {
